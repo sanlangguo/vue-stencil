3acf5b0d5132dcdc13f7e705ac5515f697b5caff
SanLangGuo <SanLangGuo>
2019-04-14 21:46:13 +0800

    change build

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-    productionSourceMap: false,
+    // productionSourceMap: false,
     publicPath: './',
-    lintOnSave: process.env.NODE_ENV !== 'prod',
-    devServer: {
-        overlay: {
-          warnings: false,
-          errors: false
-        }
-    },
     css: {
         extract: true,
         sourceMap: false,
