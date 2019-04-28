ee554633865584a643a426795eeac7114a0649f8
runman <781611293@qq.com>
2019-04-28 11:36:03 +0800

    add js file

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-    // productionSourceMap: false,
-    publicPath: './',
-    css: {
-        extract: true,
-        sourceMap: false,
-        loaderOptions: {},
-        modules: false
+  productionSourceMap: false,
+  publicPath: './',
+  css: {
+      extract: true,
+      sourceMap: false,
+      loaderOptions: {},
+      modules: false
+  },
+  configureWebpack: {
+    output: {
+      filename: 'javascript/[name].[hash].js',
+      chunkFilename: 'javascript/[name].[hash].js'
     }
+  },
 };
