5518115c6a22a9e13addfb751be0a24584970209
sanlangguo <30766629+sanlangguo@users.noreply.github.com>
2019-10-23 14:42:09 +0800

    delete merge code

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,17 @@
 module.exports = {
-<<<<<<< HEAD
-    // productionSourceMap: false,
-    lintOnSave: false,
-    publicPath: './',
-    css: {
+   lintOnSave: false,
+   publicPath: './',
+   productionSourceMap: false,
+   css: {
         extract: true,
         sourceMap: false,
         loaderOptions: {},
         modules: false
-=======
-  productionSourceMap: false,
-  publicPath: './',
-  css: {
-      extract: true,
-      sourceMap: false,
-      loaderOptions: {},
-      modules: false
-  },
+   },
   configureWebpack: {
     output: {
       filename: 'javascript/[name].[hash].js',
       chunkFilename: 'javascript/[name].[hash].js'
->>>>>>> ee554633865584a643a426795eeac7114a0649f8
     }
   },
 };
